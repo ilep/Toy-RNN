0b97662aeabb4acd132e69109fd6c4f426f710cc
klefay <lefay.kevin@gmail.com>
2020-04-02 21:48:05 +0200

    Update vanilla-rnn.py
    Implement the softmax function
    Add biases in the computation of the rnn_cell

--- vanilla-rnn.py
+++ vanilla-rnn.py
@@ -1,7 +1,15 @@
 import numpy as np
 
+def softmax(x):
+    """
+    Computes the softmax of the input vector.
+    
+    x: input vector (dimensions nx,1)
+    """
+    sum = np.sum(np.exp(x), axis=0)
+    return np.exp(x) / sum
 
-def rnn_cell(h, x, wxh, whh, why):
+def rnn_cell(h, x, wxh, whh, why, bh, by):
     """
     Computes on step of the RNN.
 
@@ -10,10 +18,12 @@ def rnn_cell(h, x, wxh, whh, why):
     wxh: input-hidden weights matrix (dimensions nx,nh)
     whh: hidden-hidden weights matrix (dimensions nh,nh)
     why: hidden-output weights matrix (dimensions nx,nh)
+    bh: hidden bias vector (dimensions nh,1)
+    by: output bias vector (dimensions nx,1)
     y: output vector (dimensions nx,1)
     """
-    h = np.tanh(whh * h + wxh * x)
-    y = why * h
+    h = np.tanh(np.dot(whh,h) + np.dot(wxh,x) + bh)
+    y = softmax(np.dot(why,h) + by)
     return h, y
 
 # create the dictionary
